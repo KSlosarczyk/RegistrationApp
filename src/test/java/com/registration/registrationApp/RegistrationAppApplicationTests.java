d2e460caa99e6e57ebb347f905b2f001bdd729e6
KSlosarczyk <KSlosarczyk@github.com>
2021-04-21 19:02:14 +0100

    Inital commit for Registration App Angular

---
+++ src/test/java/com/registration/registrationApp/RegistrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration.registrationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
